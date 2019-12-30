0c4d92dc8370e4267e4ff0adbc1f9aa53402c984
tsunemoto1098 <t-kosuke666@au.com>
2019-12-30 12:28:00 +0900

    グループ機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get 'notes/index'
   root "notes#index"
   resources :users, only: [:edit, :update]
-
+  resources :groups, only: [:new, :create, :edit, :update]
   
 end
