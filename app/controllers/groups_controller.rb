c47fa3b51e07b6cb30bf4450f2e117f21cc72398
tsunemoto1098 <t-kosuke666@au.com>
2020-01-01 23:59:34 +0900

    トップページのレイアウト変更、バリデーションの追加、各表示名の変更等

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -15,11 +15,13 @@ class GroupsController < ApplicationController
   end
 
   def edit
+    @group = Group.find(params[:id])
   end
 
   def update
+    @group = Group.find(params[:id])
     if @group.update(group_params)
-      redirect_to root_path
+      redirect_to group_notes_path(@group)
     else
       render :edit
     end
