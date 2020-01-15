9dbbdb04289d8460346bd605afda941589bf7948
tsunemoto1098 <t-kosuke666@au.com>
2020-01-15 16:26:57 +0900

    記述微修正

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,9 +2,9 @@ class Group < ApplicationRecord
   has_many :user_groups
   has_many :users, through: :user_groups
   has_many :notes, dependent: :destroy
+
   validates :name, presence: true, uniqueness: true
-  validates :name,
-    length: { minimum: 1, maximum: 10 }
+  validates :name,length: { minimum: 1, maximum: 10 }
 
   def show_last_note
     if (last_note = notes.last).present?
