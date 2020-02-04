0590e01e624e141a4d12a58850b4efe0b6699584
tsunemoto1098 <t-kosuke666@au.com>
2020-02-04 18:12:10 +0900

    user検索、追加機能実装

---
+++ app/assets/javascripts/user.js
@@ -0,0 +1,72 @@
+$(function() {
+  function addUser(user) {
+    let html = `
+      <div class="group_user clearfix">
+        <p class="group_user_name">${user.name}</p>
+        <div class="user-search-add group_user_btn group_user_btn_add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+      </div>
+    `;
+    $("#user-search-result").append(html);
+  }
+
+  function addNoUser() {
+    let html = `
+      <div class="group_user clearfix">
+        <p class="group_user_name">ユーザーが見つかりません</p>
+      </div>
+    `;
+    $("#user-search-result").append(html);
+  }
+  function addDeleteUser(name, id) {
+    let html = `
+    <div class="group_user clearfix" id="${id}">
+      <p class="group_user_name">${name}</p>
+      <div class="user-search-remove group_user_btn group_user_btn_remove js-remove-btn" data-user-id="${id}" data-user-name="${name}">解除</div>
+    </div>`;
+    $(".js-add-user").append(html);
+  }
+  function addMember(userId) {
+    let html = `<input value="${userId}" name="group[user_ids][]" type="hidden" id="group_user_ids_${userId}" />`;
+    $(`#${userId}`).append(html);
+  }
+  $("#user-search-field").on("keyup", function() {
+    let input = $("#user-search-field").val();
+    $.ajax({
+      type: "GET",
+      url: "/users",
+      data: { keyword: input },
+      dataType: "json"
+    })
+      .done(function(users) {
+        $("#user-search-result").empty();
+
+        if (users.length !== 0) {
+          users.forEach(function(user) {
+            addUser(user);
+          });
+        } else if (input.length == 0) {
+          return false;
+        } else {
+          addNoUser();
+        }
+      })
+      .fail(function() {
+        alert("通信エラーです。ユーザーが表示できません。");
+      });
+  });
+  $(document).on("click", ".group_user_btn_add", function() {
+    console.log
+    const userName = $(this).attr("data-user-name");
+    const userId = $(this).attr("data-user-id");
+    $(this)
+      .parent()
+      .remove();
+    addDeleteUser(userName, userId);
+    addMember(userId);
+  });
+  $(document).on("click", ".group_user_btn_remove", function() {
+    $(this)
+      .parent()
+      .remove();
+  });
+});
